a0d9a850f25a84d4c5f4aca7428a6acd4911dd8a
antlampas <francescoantonetti1@gmail.com>
2023-12-05 22:05:14 +0100

    -

--- src/board/isMoveValid.cpp
+++ src/board/isMoveValid.cpp
@@ -7,6 +7,7 @@
 #include <regex>
 #include <cmath>
 
+
 #ifndef BOARD_HPP
 #include "board.hpp"
 #endif
@@ -14,7 +15,7 @@
 bool board::isMoveValid(std::string start,std::string end)
 {
     std::string piece {this->getPieceInSquare(start)};
-    
+
     if(this->isPieceNameValid(piece) && this->isCoordinateValid(start) && this->isCoordinateValid(end))
     {
         std::pair<int,int> startCoordinates = this->decodeCoordinates(start);
@@ -26,19 +27,19 @@ bool board::isMoveValid(std::string start,std::string end)
         const int& endColumn   {endCoordinates.second};
         
         bool advanceOne   = ((startColumn == endColumn) && ((endRow - startRow) == 1));
-        bool moveHV       = (!(((startColumn != endColumn) && (startRow == endRow)) != !((startColumn == endColumn) && (startRow != endRow))));
+        bool moveHV       = (!((startColumn != endColumn) && (startRow == endRow)) != !((startColumn == endColumn) && (startRow != endRow)));
         bool moveDiagonal = (std::abs(endRow-startRow) == std::abs(endColumn-startColumn));
         bool moveAnywhere = ((!moveHV != !moveDiagonal) || ((startRow == endRow) && (startColumn == endColumn)));
         bool moveOne      = ((std::abs(endRow-startRow) == 1) || (std::abs(endColumn-startColumn) == 1));
-        bool moveL        = (!(std::abs(endRow-startRow) == 1) && (std::abs(endColumn-startColumn) == 2)) != !((std::abs(endRow-startRow) == 2) && (std::abs(endColumn-startColumn) == 1));
+        bool moveL        = (!((std::abs(endRow-startRow) == 1) && (std::abs(endColumn-startColumn) == 2)) != !((std::abs(endRow-startRow) == 2) && (std::abs(endColumn-startColumn) == 1)));
         bool noMove       = ((startRow == endRow) && (startColumn == endColumn));
         
-        if(piece.find("p"))      return noMove || advanceOne;
-        else if(piece.find("r")) return noMove || moveHV;
-        else if(piece.find("n")) return noMove || moveL;
-        else if(piece.find("b")) return noMove || moveDiagonal;
-        else if(piece.find("q")) return noMove || moveAnywhere;
-        else if(piece.find("k")) return noMove || moveOne;
+        if(piece.at(1) == 'p')      return (noMove || advanceOne);
+        else if(piece.at(1) == 'r') return (noMove || moveHV);
+        else if(piece.at(1) == 'n') return (noMove || moveL);
+        else if(piece.at(1) == 'b') return (noMove || moveDiagonal);
+        else if(piece.at(1) == 'q') return (noMove || moveAnywhere);
+        else if(piece.at(1) == 'k') return (noMove || moveOne);
         else
         {
             this->error = "Move validation: something went wrong...";
--- src/board/move.cpp
+++ src/board/move.cpp
@@ -3,7 +3,7 @@
  * FALP Chess © 2023 by Antlampas is licensed under CC BY-SA 4.0. To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/
  *
  */
-
+#include <iostream>
 #ifndef BOARD_HPP
 #include "board.hpp"
 #endif
