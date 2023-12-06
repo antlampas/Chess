2e3cb4eb915354068c7d6f32e0469c83c4c98e63
antlampas <francescoantonetti1@gmail.com>
2023-12-06 14:28:17 +0100

    -

--- src/board/listValidMoves.cpp
+++ src/board/listValidMoves.cpp
@@ -16,37 +16,61 @@ std::vector<std::string> board::listValidMoves(std::string coordinates)
     const int& column {decodedCoordinates.second};
     
     const std::string& piece = this->getPieceInSquare(coordinates);
-    
+    std::vector<std::string> movesList;
+
     if(this->isPieceNameValid(piece))
     {
-        std::vector<std::string> movesList;
         if(piece.at(1)=='p')
         {
             if(piece.at(0) == 'w')
             {
-                if((((row+1)<9)&&((column-1)<9)))
+                if((((row+1)<9)&&((column-1)>-1)))
                 {
-                    std::string square = this->encodeCoordinates(std::pain<int,int>(row+1,column-1));
-                    if((this->getPieceInSquare(square) == 'e') || (this->getPieceInSquare(square).at(0) != piece.at(0)))
-                        moveList.append(this->encodeCoordinates(std::pain<int,int>(row+1,column-1)));
+                    std::string square = this->encodeCoordinates(std::pair<int,int>(row+1,column-1));
+                    if((this->getPieceInSquare(square) == "e") || (this->getPieceInSquare(square).at(0) != piece.at(0)))
+                        movesList.push_back(square);
                 }
-                else if((((row+1)<9)&&((column+1)<9)))
+                if((((row+1)<9)&&((column+1)<9)))
                 {
-                    std::string square = this->encodeCoordinates(std::pain<int,int>(row+1,column+1));
-                    if((this->getPieceInSquare(square) == 'e') || (this->getPieceInSquare(square).at(0) != piece.at(0)))
-                        moveList.append(this->encodeCoordinates(std::pain<int,int>(row+1,column+1)));
+                    std::string square = this->encodeCoordinates(std::pair<int,int>(row+1,column+1));
+                    if((this->getPieceInSquare(square) == "e") || (this->getPieceInSquare(square).at(0) != piece.at(0)))
+                        movesList.push_back(square);
                 }
-                else if((((row+1)<9))){}
+                if((((row+1)<9)))
+                {
+                    std::string square = this->encodeCoordinates(std::pair<int,int>(row+1,column));
+                    if((this->getPieceInSquare(square) == "e") || (this->getPieceInSquare(square).at(0) != piece.at(0)))
+                        movesList.push_back(square);
+                }
             }
-            else if(piece.at(0) == 'b'){}
+            else if(piece.at(0) == 'b')
+            {
+                if((((row-1)>-1)&&((column-1)>-1)))
+                {
+                    std::string square = this->encodeCoordinates(std::pair<int,int>(row-1,column-1));
+                    if((this->getPieceInSquare(square) == "e") || (this->getPieceInSquare(square).at(0) != piece.at(0)))
+                        movesList.push_back(square);
+                }
+                if((((row-1)>-1)&&((column+1)<9)))
+                {
+                    std::string square = this->encodeCoordinates(std::pair<int,int>(row-1,column+1));
+                    if((this->getPieceInSquare(square) == "e") || (this->getPieceInSquare(square).at(0) != piece.at(0)))
+                        movesList.push_back(square);
+                }
+                if((((row-1)>-1)))
+                {
+                    std::string square = this->encodeCoordinates(std::pair<int,int>(row-1,column));
+                    if((this->getPieceInSquare(square) == "e") || (this->getPieceInSquare(square).at(0) != piece.at(0)))
+                        movesList.push_back(square);
+                }
+            }
         }
         else if(piece.at(1)=='r'){}
         else if(piece.at(1)=='n'){}
         else if(piece.at(1)=='b'){}
         else if(piece.at(1)=='q'){}
         else if(piece.at(1)=='k'){}
-        else return std::vector<std::string>();
-        return moveList;
+        return movesList;
     }
     else
         return std::vector<std::string>();
