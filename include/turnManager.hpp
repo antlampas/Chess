233c11632fb1f6bb4d5c32d3d8d05d3c6c6e6cfe
antlampas <francescoantonetti1@gmail.com>
2023-12-11 22:18:39 +0100

    -

---
+++ include/turnManager.hpp
@@ -0,0 +1,23 @@
+/*
+ *
+ * FALP Chess © 2023 by Antlampas is licensed under CC BY-SA 4.0. To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/
+ *
+ */
+
+#ifndef TURNMANAGER_HPP
+#define TURNMANAGER_HPP
+
+#include "board.hpp"
+
+#ifdef DEBUG
+    #define private public
+#endif
+
+class turnManager
+{
+    board* b;
+    bool team;
+    
+}
+
+#endif
