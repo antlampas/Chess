c4bded463647b516930394bba82d5ae1d24a5c42
antlampas <francescoantonetti1@gmail.com>
2023-12-11 14:57:17 +0100

    -

--- src/board/board.cpp
+++ src/board/board.cpp
@@ -13,47 +13,21 @@
 
 board::board()
 {
-    std::for_each(this->boardMap.begin(),this->boardMap.end(),[](std::vector<std::string> column){std::fill(column.begin(),column.end(),"e");});
+    const std::vector<std::string> whiteBackLine  {"wr","wn","wb","wq","wk","wb","wn","wr"};
+    const std::vector<std::string> blackBackLine  {"br","bn","bb","bq","bk","bb","bn","br"};
+    const std::vector<std::string> whitePawnLine  {"wp","wp","wp","wp","wp","wp","wp","wp"};
+    const std::vector<std::string> blackPawnLine  {"bp","bp","bp","bp","bp","bp","bp","bp"};
+    const std::vector<std::string> emptyLine      {"e", "e", "e", "e", "e", "e", "e", "e" };
 
-    const std::vector<std::string> backLine {"r","n","b","q","k","b","n","r"};
+    std::for_each(this->boardMap.begin(),this->boardMap.end(),[emptyLine](std::vector<std::string> column){std::copy(emptyLine.begin(),emptyLine.end(),column.begin());});
     
-    std::vector<std::string> line {};
-    
-    pieceType team = "w";
-
     for(int row: {0,1,2,3,4,5,6,7})
     {
-        line.clear();
-        if(row==0)
-        {
-            team = "w";
-            for(int column: {0,1,2,3,4,5,6,7})
-                line.push_back(team+backLine.at(column));
-        }
-        else if(row==7)
-        {
-            team = "b";
-            for(int column: {0,1,2,3,4,5,6,7})
-                line.push_back(team+backLine.at(column));
-        }
-        else if(row==1)
-        {
-            team = "w";
-            for(int column: {0,1,2,3,4,5,6,7})
-                line.push_back(team+"p");
-        }
-        else if(row==6)
-        {
-            team = "b";
-            for(int column: {0,1,2,3,4,5,6,7})
-                line.push_back(team+"p");
-        }
-        else
-            for(int column: {0,1,2,3,4,5,6,7})
-                line.push_back("e");
-        
-        for(int column: {0,1,2,3,4,5,6,7})
-                this->boardMap.at(row).at(column) = line.at(column);
+        if(row==0)      std::copy(whiteBackLine.begin(),whiteBackLine.end(),this->boardMap.at(row).begin());
+        else if(row==7) std::copy(blackBackLine.begin(),blackBackLine.end(),this->boardMap.at(row).begin());
+        else if(row==1) std::copy(whitePawnLine.begin(),whitePawnLine.end(),this->boardMap.at(row).begin());
+        else if(row==6) std::copy(blackPawnLine.begin(),blackPawnLine.end(),this->boardMap.at(row).begin());
+        else            std::copy(emptyLine.begin(),emptyLine.end(),this->boardMap.at(row).begin());
     }
 }
 
@@ -61,22 +35,17 @@ board::board(boardMapType initialPositions)
 {
     std::regex pieceNameFormat("^[wb][prnbkq]$|^e$");
     
+    const std::vector<std::string> emptyLine {"e","e","e","e","e","e","e","e"};
+
     bool validInput = true;
 
     for(auto row: initialPositions)
-    {
         for(auto column: row)
             if(!std::regex_match(column,pieceNameFormat))
-            {
                 validInput = false;
-                break;
-            }
-        if(!validInput)
-        {
-            std::for_each(this->boardMap.begin(),this->boardMap.end(),[](columnType column){std::fill(column.begin(),column.end(),"e");});
-            return;
-        }
-    }
 
-    std::swap(this->boardMap,initialPositions);
+    if(validInput)
+        std::copy(initialPositions.begin(),initialPositions.end(),this->boardMap.begin());
+    else
+        std::for_each(this->boardMap.begin(),this->boardMap.end(),[emptyLine](columnType column){std::copy(emptyLine.begin(),emptyLine.end(),column.begin());});
 }
