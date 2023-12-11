af23834d206e6563d18a8023007714b2be55f1a8
antlampas <francescoantonetti1@gmail.com>
2023-12-11 13:03:41 +0100

    -

--- src/board/board.cpp
+++ src/board/board.cpp
@@ -16,20 +16,45 @@ board::board()
     std::for_each(this->boardMap.begin(),this->boardMap.end(),[](std::vector<std::string> column){std::fill(column.begin(),column.end(),"e");});
 
     const std::vector<std::string> backLine {"r","n","b","q","k","b","n","r"};
+    
+    std::vector<std::string> line {};
+    
+    pieceType team = "w";
 
     for(int row: {0,1,2,3,4,5,6,7})
+    {
+        line.clear();
         if(row==0)
+        {
+            team = "w";
             for(int column: {0,1,2,3,4,5,6,7})
-                this->boardMap.at(row).at(column) = "w"+backLine.at(column);
+                line.push_back(team+backLine.at(column));
+        }
         else if(row==7)
+        {
+            team = "b";
             for(int column: {0,1,2,3,4,5,6,7})
-                this->boardMap.at(row).at(column) = "b"+backLine.at(column);
+                line.push_back(team+backLine.at(column));
+        }
         else if(row==1)
+        {
+            team = "w";
             for(int column: {0,1,2,3,4,5,6,7})
-                this->boardMap.at(row).at(column) = "wp";
+                line.push_back(team+"p");
+        }
         else if(row==6)
+        {
+            team = "b";
             for(int column: {0,1,2,3,4,5,6,7})
-                this->boardMap.at(row).at(column) = "bp";
+                line.push_back(team+"p");
+        }
+        else
+            for(int column: {0,1,2,3,4,5,6,7})
+                line.push_back("e");
+        
+        for(int column: {0,1,2,3,4,5,6,7})
+                this->boardMap.at(row).at(column) = line.at(column);
+    }
 }
 
 board::board(boardMapType initialPositions)
