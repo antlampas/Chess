17abc02812f4bd7f00cc76149a8f52840b5280cb
antlampas <francescoantonetti1@gmail.com>
2023-12-12 11:38:36 +0100

    -

--- src/turnManager/getInstance.cpp
+++ src/turnManager/getInstance.cpp
@@ -1,5 +1,15 @@
+/*
+ *
+ * FALP Chess © 2023 by Antlampas is licensed under CC BY-SA 4.0. To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/
+ *
+ */
+
+#ifndef TURNMANAGER_HPP
+#include "turnManager.hpp"
+#endif
+
 static turnManager& turnManager::getInstance(board& b)
 {
-    static turnManager instance(&b);
+    static turnManager instance(b);
     return instance;
 }
