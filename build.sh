7dbbe1fe85c5afb43eb6c06411d2cdc28ced27f6
antlampas <francescoantonetti1@gmail.com>
2023-12-14 22:54:08 +0100

    -

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# if [[ -d build ]]
-# then
-#     rm -rvf build/*
-# else
-#     mkdir build
-# fi
+if ! [[ -d build ]]
+then
+    mkdir build
+fi
 cd build
 cmake .. -DCMAKE_BUILD_TYPE="$1" -DLIBRARIES_LINKING="$2" -DTEST="$3" && make
 cd ..
