587e54441483b03b32458be3421af1836936dc35
antlampas <francescoantonetti1@gmail.com>
2023-12-15 09:43:11 +0100

    -

---
+++ src/timer/getRemainingTime.cpp
@@ -0,0 +1,11 @@
+/*
+ *
+ * FALP Chess © 2023 by Antlampas is licensed under CC BY-SA 4.0. To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/
+ *
+ */
+
+#ifndef TIMER_CPP
+#include "timer.cpp"
+#endif
+
+std::chrono::duration timer::getRemainingTime(){}
