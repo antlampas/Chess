649806c2ec96ecf21f1813f3c7ae412f513a3b95
antlampas <francescoantonetti1@gmail.com>
2023-12-17 17:25:18 +0100

    -

--- src/timer/getElapsedTime.cpp
+++ src/timer/getElapsedTime.cpp
@@ -11,6 +11,6 @@
 std::chrono::duration<long int,std::chrono::seconds> timer::getElapsedTime()
 {
     std::chrono::time_point<std::chrono::steady_clock,std::chrono::seconds> = std::chrono::time_point_cast<std::chrono::seconds>(std::chrono::steady_clock::now());
-    auto d = std::chrono::time_point_cast<std::chrono::seconds>(std::chrono::steady_clock::now()) - this->startTime;
+    std::chrono::duration<long int,std::chrono::seconds> d = std::chrono::time_point_cast<std::chrono::seconds>(std::chrono::steady_clock::now()) - this->startTime;
     return std::chrono::duration_cast<std::chrono::seconds>(d);
 }
