c01b533101ccc77ce2e4f95ddc2cb842bd7d9c94
antlampas <francescoantonetti1@gmail.com>
2023-12-24 23:01:27 +0100

    -

--- src/timer/startTimer.cpp
+++ src/timer/startTimer.cpp
@@ -10,24 +10,24 @@
 
 template<typename T,typename U> bool timer::startTimer(T f,U* obj,std::future<void> exitSignal)
 {
-    std::function<void(std::function<void()>*,std::future<void>)> function = std::move([this](std::function<void()>* func,std::future<void> reqExit)
+    std::function<void(std::future<void>)> function = std::move([this,f](std::future<void> reqExit)
                                     {
                                         this->startTime = std::chrono::steady_clock::now();
                                         this->stopTime  = this->startTime + this->interval;
                                         std::chrono::duration<long int> elapsedTime {std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - this->startTime)};
                                         while(elapsedTime < this->interval)
                                         {
-                                            std::this_thread::sleep_for(std::chrono::milliseconds(998))
+                                            std::this_thread::sleep_for(std::chrono::milliseconds(998));
                                             if(reqExit.wait_for(std::chrono::milliseconds(1))==std::future_status::ready)
                                                 break;
                                             elapsedTime = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - this->startTime);
                                         }
-                                        f();
+                                        func();
                                     });
     //std::thread func = std::thread(function,&f,dynamic_cast<obj_t> obj,std::move(exitSignal));
 
     if(func.get_id() != std::thread::id{})
-        this->callback = std::move(std::thread(function,&f,obj,std::move(exitSignal)));
+        this->callback = std::move(std::thread(function,obj,std::move(exitSignal)));
 
     if(this->callback.get_id() != std::thread::id{})
         return true;
