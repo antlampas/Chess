34083a47dba763ffd72526ba2c45bcc29d94fdf1
antlampas <francescoantonetti1@gmail.com>
2023-12-25 19:25:30 +0100

    -

--- include/timer.hpp
+++ include/timer.hpp
@@ -30,9 +30,9 @@ class timer
     timer(long int);
     ~timer();
     void setInterval(std::chrono::duration<long int>);
-    template<typename T,typename U> bool startTimer(T F,U* obj,std::future<void> exitSignal)
+    template<typename T,typename U> bool startTimer(T f(),U* obj,std::future<void> exitSignal)
     {
-        std::function<void(U*,T,std::future<void>)> function = std::move([this](U* object,T f,std::future<void> reqExit)
+        std::function<void(std::future<void>)> function = std::move([this](std::future<void> reqExit)
         {
             this->startTime = std::chrono::steady_clock::now();
             this->stopTime  = this->startTime + this->interval;
@@ -44,7 +44,7 @@ class timer
                     break;
                 elapsedTime = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - this->startTime);
             }
-            (*f)();
+            obj->f();
         });
 
         this->callback = std::move(std::thread(function,std::move(F),std::move(obj),std::move(exitSignal)));
